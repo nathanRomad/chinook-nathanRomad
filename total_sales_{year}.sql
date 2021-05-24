e43a537597bc44a12d80bc356baa40bae848e6d8
nathanRomad <nathan.a.hamilton@gmail.com>
2021-05-24 15:00:37 -0500

    fix: total_sales_{year}, added addition query

--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -1,4 +1,11 @@
+-- SELECT SUM (Total)
+-- FROM Invoice
+-- WHERE InvoiceDate BETWEEN "2009-01-01" AND "2009-12-31"
+-- OR InvoiceDate BETWEEN "2011-01-01" AND "2011-12-31"
+-- GROUP BY strftime ('%Y', InvoiceDate)
+
+
 SELECT SUM (Total)
 FROM Invoice
-WHERE InvoiceDate BETWEEN "2009-01-01" AND "2009-12-31"
-OR InvoiceDate BETWEEN "2011-01-01" AND "2011-12-31"
+WHERE strftime ( '%Y', InvoiceDate ) = '2009' OR strftime ( '%Y', InvoiceDate ) = '2011'
+GROUP BY strftime ( '%Y', InvoiceDate )
