e0c2b4348d25ba5101ed90d6054d96feca79082a
nathanRomad <nathan.a.hamilton@gmail.com>
2021-05-25 15:16:10 -0500

    build: Which sales agent made the most in sales over all?

--- top_agent.sql
+++ top_agent.sql
@@ -1,2 +1,7 @@
-SELECT Employee.FirstName, Employee.LastName, SUM(Total)
-FROM 
+SELECT MAX(Total), EmployeeName
+FROM (SELECT Employee.FirstName ||' '||Employee.LastName as EmployeeName, SUM(Total) as Total
+FROM Employee
+JOIN Customer ON Employee.EmployeeId = Customer.SupportRepId
+JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
+WHERE InvoiceDate
+GROUP BY Customer.SupportRepId)
