ad5fa6f425bfe0396ea23a1e4f5c2e15cc35e6fc
nathanRomad <nathan.a.hamilton@gmail.com>
2021-05-25 11:39:03 -0500

    fix: additional solutions added to queries

--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -5,7 +5,18 @@
 -- GROUP BY strftime ('%Y', InvoiceDate)
 
 
-SELECT SUM (Total)
-FROM Invoice
-WHERE strftime ( '%Y', InvoiceDate ) = '2009' OR strftime ( '%Y', InvoiceDate ) = '2011'
-GROUP BY strftime ( '%Y', InvoiceDate )
+-- SELECT SUM (Total)
+-- FROM Invoice
+-- WHERE strftime ( '%Y', InvoiceDate ) = '2009' OR strftime ( '%Y', InvoiceDate ) = '2011'
+-- GROUP BY strftime ( '%Y', InvoiceDate )
+
+
+select
+    strftime("%Y", InvoiceDate) as iYear,
+    sum(total)
+from invoice
+where
+    iYear = "2009"
+    OR
+    iYear = "2011"
+group by iYear
