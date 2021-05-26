158c2a389ad1b84f6145f2fc86403d9e289d4f1a
nathanRomad <nathan.a.hamilton@gmail.com>
2021-05-26 09:47:43 -0500

    build: Provide a query that shows the top 5 most purchased tracks over all.

---
+++ top_5_tracks.sql
@@ -0,0 +1,42 @@
+SELECT COUNT(InvoiceLine.InvoiceId) as TrackCount, Track.Name as TrackName, Track.TrackId
+FROM InvoiceLine
+JOIN Invoice ON InvoiceLine.InvoiceId = Invoice.InvoiceId
+JOIN Track ON InvoiceLine.TrackId = Track.TrackId
+GROUP BY Track.TrackId
+ORDER BY TrackCount ASC
+LIMIT 5
+
+
+---------CALEB----------
+SELECT COUNT(InvoiceId) as InvoiceCount, TrackId
+FROM InvoiceLine
+GROUP BY TrackId
+ORDER BY InvoiceCount DESC
+LIMIT 5
+
+-------BLU---------
+select
+
+	track.Name as trackName,
+	count(*) as totalTracks,
+    track.trackId
+
+from InvoiceLine
+
+join track on track.TrackId = InvoiceLine.TrackId
+
+group by InvoiceLine.TrackId
+order by totalTracks desc
+limit 5
+
+-------------AMART-----------
+With TrackCounts AS (
+    Select COUNT(InvoiceLineId) TotalSales, TrackId
+    FROM InvoiceLine
+    GROUP BY TrackId
+)
+SELECT tc.TotalSales TotalSales, t.Name
+FROM TrackCounts tc
+JOIN Track t ON tc.TrackId = t.TrackId
+ORDER BY TotalSales DESC
+LIMIT 5
